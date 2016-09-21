2bab59675149fbbf69a173aa04be830a2a71f5fb
Elizabeth Kleinschmidt <tenlia@hotmail.com>
2016-09-20 18:48:59 -0700

    Claudia helped me out a bunch

--- lab-elizabeth/server.js
+++ lab-elizabeth/server.js
@@ -1,1 +1,68 @@
 'use strict';
+
+// node modules
+const fs = require('fs');
+const http = require('http');
+const url = require('url');
+const queryString = require('querystring');
+
+// npm modules
+const cowsay = require('cowsay');
+
+// app modules
+const parseBody = require('./lib/parse-body');
+
+// module constants
+const PORT = process.env.PORT || 3000;
+
+// module logic
+const server = http.createServer(function(req, res){
+  req.url = url.parse(req.url);
+  req.url.query = queryString.parse(req.url.query);
+
+  if(req.url.pathname === '/'){
+    if(req.url.query.text){
+      res.writeHead(200, {'Content-Type': 'text/plain'});
+      res.write(req.url.query.text);
+    } else {
+      res.writeHead(200, {'Content-Type': 'text/plain'});
+      res.write('hello world');
+    }
+    res.end();
+    return;
+  }
+
+  if(req.method === 'GET' && req.url.pathname === '/cowsay'){
+    parseBody(req, function(err){
+      if(err) return console.error(err);
+      if(req.url.query.text){
+        res.writeHead(200, ({'Content-Type': 'text/plain'}));
+        res.write(cowsay.say({
+          text: req.url.query.text,
+          f: req.url.query.f,
+          e: req.url.query.e,
+          T: req.url.query.t,
+        }));
+      } else {
+        res.writeHead(400, ({'Content-Type': 'text/plain'}));
+        res.write(cowsay.say({
+          text: 'bad request\ntry: http :3000/cowsay text==howdy',
+          f: 'vader',
+        }));
+      }
+      res.end();
+      return;
+    });
+  }
+
+  res.writeHead(400, {'Content-Type': 'text/plain'});
+  res.write(cowsay.say({
+    text: 'bad request\nplease try again.',
+    f: 'vader-koala',
+  }));
+  res.end();
+});
+
+server.listen(PORT, function(){
+  console.log('server up! <(0-0)> ', PORT);
+});
